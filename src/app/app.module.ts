e0a054f3b63cf75a5a5feeea5cbe1d67eb18aa05
mali84211 <mali84211@gmail.com>
2024-07-14 18:32:34 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {HttpClientModule} from "@angular/common/http";
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import { SerchPipe } from './serch.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SerchPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CanvasJSAngularChartsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
